fac6d924381dc669a780d1ad9c748a897108d4be
Paul Thoren <paul.thoren@gmail.com>
2024-09-22 23:30:50 -0400

    update remaining categories and exercises

--- git_fit.py
+++ git_fit.py
@@ -1,22 +1,20 @@
-from abc import ABC, abstractmethod
-from datetime import datetime, timedelta
-import json
-import random
-import os
-import csv
-import sys
-import subprocess
 
+
+from abc import ABC, abstractmethod
 from dataclasses import dataclass, field, asdict
+from datetime import datetime, timedelta
 from typing import List, Dict
+import csv
+import json
+import os
+import random
 
 @dataclass
-class Log:
+class ExerciseLog:
     file_path: str = 'log.csv'
     headers: List[str] = field(default_factory=lambda: ['timestamp', 'category', 'exercise', 'reps'])
 
     def __post_init__(self):
-        # Ensure the file exists and has the correct headers
         if not os.path.exists(self.file_path):
             self._initialize_log_file()
 
@@ -27,10 +25,8 @@ class Log:
             writer.writerow(self.headers)
 
     def record(self, category: str, exercise: str, reps: int):
-        # Create a new log entry with the current timestamp
         new_entry = [datetime.now().strftime('%Y-%m-%d %H:%M:%S'), category, exercise, str(reps)]
 
-        # Read the existing log
         with open(self.file_path, mode='r') as file:
             existing_entries = list(csv.reader(file))
 
@@ -43,7 +39,6 @@ class Log:
             writer.writerows(updated_entries)
 
     def load_log(self) -> List[List[str]]:
-        # Load the log from CSV
         if os.path.exists(self.file_path):
             with open(self.file_path, mode='r') as file:
                 return list(csv.reader(file))
@@ -51,15 +46,34 @@ class Log:
             print("Log file not found.")
             return []
 
+
+@dataclass
+class State:
+    remaining_categories: List[str]
+    remaining_exercises: Dict[str, List[str]]
+    last_executed: str = ""
+
+    def save(self) -> None:
+        with open('.state.json', 'w') as file:
+            json.dump(asdict(self), file, indent=4)
+
+    @staticmethod
+    def load(config):
+        try:
+            with open(".state.json", 'r') as file:
+                state_dict = json.load(file)
+                return State(**state_dict)
+        except FileNotFoundError:
+            return State(remaining_categories=list(config.categories.keys()), remaining_exercises=config.categories)
+
 class Routine(ABC):
     @abstractmethod
     def next_exercise(self) -> str:
         pass
 
     @abstractmethod
-    def record(self, category: str, exercise: str):
+    def record(self, state: State, category: str, exercise: str, reps: float):
         pass
-
 
 @dataclass
 class Cooldown:
@@ -81,10 +95,6 @@ class Config:
     routine: Routine
     categories: Dict[str, List[str]]
 
-    def save(self, file_path: str) -> None:
-        with open(file_path, 'w') as file:
-            json.dump(asdict(self), file, indent=4)
-
     @staticmethod
     def load():
         with open('config.json', 'r') as file:
@@ -95,30 +105,11 @@ class Config:
 
         return Config(cooldown=cooldown, routine=routine, categories=config['categories'])
 
-
-@dataclass
-class State:
-    remaining_categories: List[str]
-    remaining_exercises: Dict[str, List[str]]
-    last_executed: str = ""
-
-    def save(self) -> None:
-        with open('.state.json', 'w') as file:
-            json.dump(asdict(self), file, indent=4)
-
-    @staticmethod
-    def load(config: Config):
-        try:
-            with open(".state.json", 'r') as file:
-                state_dict = json.load(file)
-                return State(**state_dict)
-        except FileNotFoundError:
-            return State(remaining_categories=list(config.categories.keys()), remaining_exercises=config.categories)
-
 class RandomCycle(Routine):
     def next_exercise(self, config: Config, state: State) -> str:
         if len(state.remaining_categories) == 0:
             state.remaining_categories = list(config.categories.keys())
+
         category = random.choice(state.remaining_categories)
 
         if len(state.remaining_exercises[category]) == 0:
@@ -129,20 +120,21 @@ class RandomCycle(Routine):
 
         return category, exercise
 
-    def record(self, category, exercise):
-        return super().record(category, exercise)
+    def record(self, state, category, exercise, reps):
+        state.remaining_exercises[category].remove(exercise)
+        state.remaining_categories.remove(category)
 
 class FourDaySplit(Routine):
     def next_exercise(self) -> str:
         return super().get_next_exercise()
 
-    def record(self, category, exercise):
+    def record(self, state, category, exercise):
         return super().record(category, exercise)
 
 def main():
     config = Config.load()
     state = State.load(config)
-    log = Log()
+    log = ExerciseLog()
 
     if not config.cooldown.check(state.last_executed):
         print("cooldown")
@@ -157,8 +149,11 @@ def main():
 
     reps = int(input("How many reps did you do?: "))
     if (reps > 0):
-        routine.record(category, exercise)
+        routine.record(state, category, exercise)
         log.record(category, exercise, reps)
+        state.save()
+        print('Remaining categories:', state.remaining_categories)
+        print('Remaining exercises:', state.remaining_exercises[category])
 
 if __name__ == "__main__":
     main()
